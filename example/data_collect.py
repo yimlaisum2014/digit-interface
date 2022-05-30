a5a2b7f2901807c745e6fcec9bb083f5b8659221
yimlaisum2014 <yimlaisum2014@gmail.com>
2022-05-29 22:11:45 -0400

    update data_collect.py

--- example/data_collect.py
+++ example/data_collect.py
@@ -1,28 +1,52 @@
-import sys
 import os
+import sys
 import time
-from datetime import datetime
-from digit_interface.digit import Digit
-from digit_interface.digit_handler import DigitHandler
 
-class Collect ():
+import click
+
+from digit_interface.digit import Digit
+
+
+class Collect(object):
     def __init__(self):
         # global variable
         self.trial = None
-        self.step = None
+        self.sample = None
 
         # connect DIGIT
         self.digit_right = Digit("D20356", "Right Gripper")
         self.digit_left = Digit("D20365", "Left Gripper")
 
-        self.digit_left.connect()
-        self.digit_right.connect()
+        # self.digit_left.connect()
+        # self.digit_right.connect()
 
-        self.intensity(int(5)) #Under bright env. may not too high
-        print("Start collecting")
-        self.collect()
+        self.connect(self.digit_left)
+        self.connect(self.digit_right)
 
-    def intensity(self,intensity):
+        # self.intensity(int(5)) #Under bright env. may not too high
+
+        self.set_camera(self.digit_left)
+        self.set_camera(self.digit_right)
+
+        # print("Start collecting")
+        # self.collect()
+
+    def connect(self, camera):
+        connected = False
+        while not connected:
+            print('Try connecting....')
+            try:
+                camera.connect()
+                print('Succeed')
+                connected = True
+            except:
+                print('Failed')
+                pass
+                # import traceback
+                # print(traceback.format_exc())
+            time.sleep(1)
+
+    def intensity(self, intensity):
         # Maximum value for each channel is 15
         rgb_list = [(intensity, 0, 0), (0, intensity, 0), (0, 0, intensity)]
 
@@ -34,35 +58,78 @@ class Collect ():
         self.digit_right.set_intensity(intensity)
         time.sleep(1)
 
+    def set_camera(self, camera):
+
+        intensity = (15, 0, 0)
+        if camera is not None:
+            camera.set_intensity_rgb(*intensity)
+            # set resolution
+            camera.set_resolution(Digit.STREAMS['VGA'])
+            # set fps
+            camera.set_fps(15)
+
     def disconnect(self):
         self.digit_left.disconnect()
         self.digit_right.disconnect()
 
     def collect(self):
-        #print(sys.argv)
-        #print(type(sys.argv[1]))
+        # print(sys.argv)
+        # print(type(sys.argv[1]))
         self.trial = int(sys.argv[1])
-        self.step = int(sys.argv[2])
-        print(type(self.trial),type(self.step))
+        self.samples = int(sys.argv[2])
+        print(type(self.trial), type(self.samples))
 
         # whether having parmeters
-        if self.trial ==None or self.time == None:
-            raise Exception("There are missing parmeters -trial -time) ")
-        
+        if self.trial == None or self.samples == None:
+            raise Exception("There are missing parmeters -trial -sample) ")
+
         # make a directionay
-        savepath = "Data/_45/trial_"+ str(self.trial)+"/"
+        savepath = "Data_2/_60/trial_" + str(self.trial) + "/"
         if not os.path.exists(savepath):
             os.makedirs(savepath)
 
         # saving images
-        for i in range(self.step):
-            self.digit_left.save_frame(path = savepath + "left_{}_{}.png".format(i))
-            self.digit_right.save_frame(path = savepath + "right_{}_{}.png".format(i))
-            time.sleep(1)
-            
+        for i in range(1, self.samples + 1):
+            self.digit_left.save_frame(path=savepath + "left_{}.png".format(i))
+            self.digit_right.save_frame(path=savepath + "right_{}.png".format(i))
+
         print("Finish collecting")
         self.disconnect()
 
 
+@click.command(help='Collect images')
+@click.option('-t', '--trial', required=True, type=click.STRING, help='trial name')
+@click.option('-s', '--samples', required=True, type=click.INT, help='Number of samples')
+@click.option('-i', '--ignore', required=False, type=click.INT, default=0, show_default=True,
+              help='Ignore first n images')
+@click.option('-o', '--output', required=True, type=click.Path(file_okay=False, dir_okay=True), help='Output folder')
+def collect(trial, samples, ignore, output):
+    c = Collect()
+    output = os.path.join(output, trial)
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    while ignore:
+        c.digit_left.get_frame()
+        c.digit_right.get_frame()
+        ignore -= 1
+
+    with click.progressbar(range(1, samples + 1), length=samples) as bar:
+        for idx in bar:
+            left = f'left_{idx}.png'
+            right = f'right_{idx}.png'
+            c.digit_left.save_frame(path=os.path.join(output, left))
+            c.digit_right.save_frame(path=os.path.join(output, right))
+            idx += 1
+
+    c.disconnect()
+
+
 if __name__ == "__main__":
-    collect = Collect()
+    #
+    # if (len(sys.argv) < 3):
+    #     print('Usage: python data_collect.py trial sample')
+    #     sys.exit(1)
+    #
+    # collect = Collect()
+    collect()
